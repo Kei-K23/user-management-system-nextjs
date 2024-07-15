8405fa141648e9518eba671dc47507e25de0b68c
Kei-K23 <keiksl2301@gmail.com>
2024-07-15 16:49:09 +0630

    Add custom hook to check component is mounted or not

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form, Input, Button, Row, Col, FormProps, Checkbox } from 'antd';
 import Link from 'next/link';
+import { useMountedState } from "react-use"
+import { useMounted } from '@/lib/use-mounted';
 
 type FieldType = {
     email: string;
@@ -12,6 +14,8 @@ type FieldType = {
 
 export default function SignInPage() {
 
+    const isMounted = useMounted();
+
     const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
         console.log('Success:', values);
     };
@@ -20,58 +24,62 @@ export default function SignInPage() {
         console.log('Failed:', errorInfo);
     };
 
-
     return (
         <div className='w-full h-full flex flex-col justify-start items-center pt-20 md:pt-32'>
             <h1 className='text-xl md:text-2xl lg:text-3xl font-bold mb-3'>Welcome Back!</h1>
             <p className='text-lg'>Login in or create new account</p>
-            <Row justify="center" className='border-2 border-neutral-100 rounded-lg w-[350px] sm:w-[500px] md:w-[600px] mt-5'>
-                <Col xs={24} sm={24} md={24} lg={24}>
-                    <Form
-                        name="basic"
-                        layout="vertical"
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
-                        className='mt-4'
-                        style={{ padding: '0 2rem' }}
-                    >
-                        <Form.Item<FieldType>
-                            label="Email"
-                            name="email"
-                            rules={[
-                                { required: true, message: 'Please input your email!' },
-                                { type: 'email', message: 'The input is not valid E-mail!' },
-                            ]}
+            {
+                isMounted ? (<Row justify="center" className='border-2 border-neutral-100 rounded-lg w-[350px] sm:w-[500px] md:w-[600px] mt-5'>
+                    <Col xs={24} sm={24} md={24} lg={24}>
+                        <Form
+                            name="basic"
+                            layout="vertical"
+                            initialValues={{ remember: true }}
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                            autoComplete="off"
+                            className='mt-4'
+                            style={{ padding: '0 2rem' }}
                         >
-                            <Input />
-                        </Form.Item>
-                        <Form.Item<FieldType>
-                            label="Password"
-                            name="password"
-                            rules={[{ required: true, message: 'Please input your password!' }]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
-                        <Form.Item>
-                            <Form.Item<FieldType> name="remember" valuePropName="checked" noStyle>
-                                <Checkbox >Remember me</Checkbox>
+                            <Form.Item<FieldType>
+                                label="Email"
+                                name="email"
+                                rules={[
+                                    { required: true, message: 'Please input your email!' },
+                                    { type: 'email', message: 'The input is not valid E-mail!' },
+                                ]}
+                            >
+                                <Input />
                             </Form.Item>
+                            <Form.Item<FieldType>
+                                label="Password"
+                                name="password"
+                                rules={[{ required: true, message: 'Please input your password!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+                            <Form.Item>
+                                <Form.Item<FieldType> name="remember" valuePropName="checked" noStyle>
+                                    <Checkbox >Remember me</Checkbox>
+                                </Form.Item>
 
-                            <Link href="/forgot-password">
-                                Forgot password
-                            </Link>
-                        </Form.Item>
-                        <Form.Item className='flex justify-between items-center'>
-                            <Button type="primary" htmlType="submit" >
-                                Log in
-                            </Button>
-                            <Link href="/sign-up" >Don&apos;t have an account! Register here</Link>
-                        </Form.Item>
-                    </Form>
-                </Col>
-            </Row>
+                                <Link href="/forgot-password">
+                                    Forgot password
+                                </Link>
+                            </Form.Item>
+                            <Form.Item className='flex justify-between items-center'>
+                                <Button type="primary" htmlType="submit" >
+                                    Log in
+                                </Button>
+                                <Link href="/sign-up" >Don&apos;t have an account! Register here</Link>
+                            </Form.Item>
+                        </Form>
+                    </Col>
+                </Row>) : (
+                    <p>loading....</p>
+                )
+            }
+
         </div>
     )
 }
---
+++ lib/use-mounted.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+
+// Check is component is mounted in client side
+export function useMounted() {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => setIsMounted(true), []);
+    return isMounted;
+}
