471d5ca93f395d1ca18622c3f899f74e373402a5
Kei-K23 <keiksl2301@gmail.com>
2024-07-15 20:21:07 +0630

    Modify the middleware to protect the routes

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,28 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+const PUBLIC_ROUTES = ['/sign-in', '/sign-up', '/forgot-password'];
+
 export function middleware(request: NextRequest) {
+    // TODO: change with actual auth cookie name
     let cookie = request.cookies.get('nextjs');
-    console.log(cookie);
-    return NextResponse.next();
+    const signInUrl = new URL('/sign-in', request.url)
+    const pathname = request.nextUrl.pathname;
+    const isPublicRoute = PUBLIC_ROUTES.includes(pathname);
+
+    // If the pathname is public then pass the authentication
+    if (isPublicRoute) {
+        return NextResponse.next();
+    }
+
+    // Check if the pathname is not public and not auth cookie exists, then redirect to sign in page
+    if (!cookie) {
+        return NextResponse.redirect(signInUrl);
+    }
 }
 
 export const config = {
-    matcher: ['/about/:path*', '/dashboard/:path*'],
+    matcher: [
+        '/((?!_next/static|_next/image|favicon.ico).*)',
+    ],
 }
